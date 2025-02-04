2465e83928f71c3c27ce149c7491cbce6ac5d670
Sunil Pawar <mail.sunilpawar@gmail.com>
2025-02-04 17:08:50 +0530

    feat: initial implementation of core features
    - Add core models (Payslip, Allowance, Deduction)
    - Implement security services
    - Add basic UI views
    - Setup SwiftData integration

--- PayslipMaxTests/Helpers/MockServices.swift
+++ PayslipMaxTests/Helpers/MockServices.swift
@@ -1,1 +1,26 @@
- 
+import Foundation
+@testable import Payslip_Max
+
+final class MockSecurityService: SecurityServiceProtocol {
+    var isInitialized: Bool = false
+    var shouldFail: Bool = false
+    
+    func initialize() async throws {
+        isInitialized = true
+    }
+    
+    func authenticate() async throws -> Bool {
+        if shouldFail {
+            throw NSError(domain: "MockError", code: -1, userInfo: nil)
+        }
+        return true
+    }
+    
+    func encrypt(_ data: Data) async throws -> Data {
+        return data
+    }
+    
+    func decrypt(_ data: Data) async throws -> Data {
+        return data
+    }
+} 
