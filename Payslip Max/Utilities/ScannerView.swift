7bf98dd7c086c31610c72fd16e6633bef3562e05
Sunil Pawar <mail.sunilpawar@gmail.com>
2025-04-03 12:30:35 +0530

    refactored Homeview & Homeviewmodel

---
+++ Payslip Max/Utilities/ScannerView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+import Vision
+import VisionKit
+
+/// A UIViewControllerRepresentable for scanning documents using VisionKit
+struct ScannerView: UIViewControllerRepresentable {
+    let onScanCompleted: (UIImage) -> Void
+    
+    func makeUIViewController(context: Context) -> VNDocumentCameraViewController {
+        let scanner = VNDocumentCameraViewController()
+        scanner.delegate = context.coordinator
+        return scanner
+    }
+    
+    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
+        let parent: ScannerView
+        
+        init(_ parent: ScannerView) {
+            self.parent = parent
+        }
+        
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+            guard scan.pageCount > 0 else { return }
+            let image = scan.imageOfPage(at: 0)
+            parent.onScanCompleted(image)
+            controller.dismiss(animated: true)
+        }
+        
+        func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+            controller.dismiss(animated: true)
+        }
+        
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+            ErrorLogger.log(error)
+            controller.dismiss(animated: true)
+        }
+    }
+} 
