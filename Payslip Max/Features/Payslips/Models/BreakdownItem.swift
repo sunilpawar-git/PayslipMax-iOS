d1306ee8c12753128379646057110e97c3e1b995
Sunil Pawar <mail.sunilpawar@gmail.com>
2025-04-08 22:49:43 +0530

    Cleaned the project muck

--- Payslip Max/Features/Payslips/Models/BreakdownItem.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-/// Represents an item in a financial breakdown (earnings or deductions)
-struct BreakdownItem: Identifiable {
-    let id = UUID()
-    let label: String
-    let value: String
-} 
